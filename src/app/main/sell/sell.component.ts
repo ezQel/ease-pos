00b6ea1128211398f3d81ef83f01d103963245a5
Ezekiel Charo <ezekiel3499@gmail.com>
2022-12-11 20:53:46 +0300

    top menu

--- src/app/main/sell/sell.component.ts
+++ src/app/main/sell/sell.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-sell',
   templateUrl: './sell.component.html',
-  styleUrls: ['./sell.component.scss']
+  styleUrls: ['./sell.component.scss'],
 })
 export class SellComponent implements OnInit {
+  products$?: Observable<any[]>;
+  products = [
+    { name: 'Haco biro pen', price: 20, stock: 12 },
+    { name: 'Bic gel pen', price: 50, stock: 10 },
+    { name: 'Oxford dictionary', price: 500, stock: 2 },
+    { name: 'Oxford Math Set', price: 400, stock: 32 },
+    { name: 'Ruler', price: 25, stock: 21 },
+    { name: 'HB Pencil', price: 30, stock: 45 },
+    { name: 'HB Pencil', price: 30, stock: 45 },
+    { name: 'HB Pencil', price: 30, stock: 45 },
+    { name: 'HB Pencil', price: 30, stock: 45 },
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.products$ = of(this.products);
   }
-
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,20 +4,25 @@ import { BottomMenuComponent } from './bottom-menu/bottom-menu.component';
 import { ProfileCardComponent } from './profile-card/profile-card.component';
 import { RouterModule } from '@angular/router';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     BottomMenuComponent,
     ProfileCardComponent,
-    SideMenuComponent
+    SideMenuComponent,
+    TopBarComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    NgbDropdownModule
   ],
   exports: [
     SideMenuComponent,
     BottomMenuComponent,
+    TopBarComponent
   ]
 })
 export class SharedModule { }
